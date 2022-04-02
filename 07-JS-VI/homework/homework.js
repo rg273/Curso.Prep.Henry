7c9dc720f67f41527e4fad05bc7222d1d042ad58
Rodrigo_LaptopI3_600U <rodrigogutierrez273@gmail.com>
2022-04-02 20:16:28 -0300

    HomeworkVI 20:17

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,23 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+
+  return nombre[0].toUpperCase() + nombre.slice(1);
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+
+  return cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +28,21 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+
+  let sumaTotal = numeros.reduce(function (account, elemento) {
+    return (account += elemento);
+  }, 0);
+  cb(sumaTotal);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+
+  array.forEach(function (elemento) {
+    cb(elemento);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +50,32 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  let nuevoArray = [];
+  nuevoArray = array.map(function (elemento) {
+    return cb(elemento);
+  });
+
+  return nuevoArray;
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  function cb(parametro) {
+    if (parametro.charAt(0) == "a") {
+      return parametro;
+    }
+  }
+  let nueboArray = [];
+
+  array.forEach(function (elemento) {
+    if (cb(elemento)) {
+      nueboArray.push(elemento);
+    }
+  });
+
+  return nueboArray;
 }
 
 // No modificar nada debajo de esta línea
@@ -53,5 +88,5 @@ module.exports = {
   sumarArray,
   forEach,
   map,
-  filter
+  filter,
 };
